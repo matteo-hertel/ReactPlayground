d9a06eb719cbb032c12a5105539b45fd09969c54
Matteo Hertel <info@matteohertel.com>
2018-11-10 18:09:48 +0000

    testing around

---
+++ slides-test/test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Box, Flex} from '@rebass/grid/';
+
+const Test = ({children}) => (
+  <Flex
+    flexDirection="column"
+    className="Papoi"
+    style={{
+      width: '100vw',
+      height: '100vw',
+      background:
+        'linear-gradient(135deg,rgb(0, 178, 214) 0%, rgb(2, 221, 216) 100%)',
+    }}>
+    <Box width={1}>{children}</Box>
+    <Flex flexDirection="row" justifyContent="space-around">
+      <Box width={1 / 2} px={2}>
+        Half width
+      </Box>
+      <Box right={0} width={1 / 2} px={2}>
+        other Half width
+      </Box>
+    </Flex>
+  </Flex>
+);
+export default Test;
--- slides-test/theme.js
+++ slides-test/theme.js
@@ -1,12 +1,30 @@
-import theme from 'mdx-deck/themes'
-
+import base from 'mdx-deck/themes';
+const green = '#42ff71';
 export default {
-  ...theme,
+  ...base,
 
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
-}
+  font:
+    '-apple-system, BlinkMacSystemFont, "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Arial", sans-serif',
+  monospace: '"IBM Plex Mono", monospace',
+  colors: {
+    text: green,
+    link: green,
+    pre: '#000',
+    preBackground: green,
+    code: '#000',
+    codeBackground: green,
+  },
+  css: {
+    background:
+      'linear-gradient(150deg, rgba(37,33,50,1) 0%, rgba(35,76,98,1) 80%)',
+    textAlign: 'left',
+    fontSize: '16px',
+    '@media screen and (min-width:64em)': {
+      fontSize: '32px',
+    },
+    '& .Slide > div': {
+      minWidth: '80vw',
+      minHeight: '60vh',
+    },
+  },
+};
